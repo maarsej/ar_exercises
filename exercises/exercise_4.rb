def4436499154ef15288d27e2491eca1e2a0285a
Nucleartom <jacob.maarse@gmail.com>
2018-04-24 02:27:47 +0000

    updating, with exercises 1-4 completed

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -1,9 +1,26 @@
-require_relative '../setup'
-require_relative './exercise_1'
-require_relative './exercise_2'
-require_relative './exercise_3'
+require_relative "../setup"
+require_relative "./exercise_1"
+require_relative "./exercise_2"
+require_relative "./exercise_3"
 
 puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+
+store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+
+store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |store|
+  puts "#{store.name} makes #{store.annual_revenue}"
+end
+
+@womens_stores_under1M = Store.where("womens_apparel = true AND annual_revenue < ?", 1000000)
+
+@womens_stores_under1M.each do |womens_store|
+  puts "#{womens_store.name} makes #{womens_store.annual_revenue}"
+end
